6f865361cbe8e3db413043375a1619db5f8e68d5
Martin Espericueta <espmartin@gmail.com>
2013-06-26 23:22:17 -0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,16 +1,16 @@
 LOCAL_PATH:= $(call my-dir)
-
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := tests
 
-LOCAL_SRC_FILES := \
-        $(call all-java-files-under, src)
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13 android-support-v4
 
-LOCAL_PACKAGE_NAME := Logcat
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13
+LOCAL_PACKAGE_NAME := logcat
+LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
 
+# Use the folloing include to make our test apk.
 include $(call all-makefiles-under,$(LOCAL_PATH))
